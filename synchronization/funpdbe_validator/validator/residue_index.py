1ab152d56328845ea14e114aad73c7b3abcee715
Andrea Soltesova <soltes.andrea@gmail.com>
2022-05-17 11:19:12 +0200

    funpdbe-validator from PDBe-KB git

--- synchronization/funpdbe_validator/validator/residue_index.py
+++ synchronization/funpdbe_validator/validator/residue_index.py
@@ -15,6 +15,7 @@ License.
 
 import json
 import requests
+import sys
 
 
 class ResidueIndexes(object):
@@ -31,12 +32,20 @@ class ResidueIndexes(object):
         # all residues in every chain are correctly indexed
     """
 
-    def __init__(self, data):
+    def __init__(self, data, mmcif_mode=False, cif_file=None):
         self.api_url = "https://www.ebi.ac.uk/pdbe/api/pdb/entry/residue_listing/"
         self.data = data
         self.pdb_id = self._set_pdb_id()
         self.mismatches = []
         self.labels = ["residues", "chains", "molecules"]
+        self.mmcif_mode=mmcif_mode
+        if self.mmcif_mode ==True :
+            if cif_file ==None :
+
+                print("Error! Please provide input mmcif file as mmcif_mode is 'True' or set mmcif_mode as 'False' to use PDBe API")
+                sys.exit(1)
+            else :
+                self.cif_file=cif_file     
 
     def check_every_residue(self):
         """
@@ -46,9 +55,16 @@ class ResidueIndexes(object):
         """
         if not self.pdb_id:
             return False
+        if self.mmcif_mode ==True : 
+            print("Getting the residue data from mmcif file instead of PDBe API")
+            self._get_mmcif_residue_list() 
         for chain_data in self.data["chains"]:
-            if not self._get_residue_numbering(chain_data):
-                return False
+            if self.mmcif_mode ==True : 
+                if not self._get_residue_numbering_from_mmcif(chain_data):
+                    return False
+            else :
+                if not self._get_residue_numbering(chain_data):
+                    return False
         return True
 
     def _set_pdb_id(self):
@@ -59,6 +75,55 @@ class ResidueIndexes(object):
         if "pdb_id" in self.data.keys():
             return self.data["pdb_id"].lower()
         return None
+
+    def _get_mmcif_residue_list(self) :
+        
+        """Get the list of residues from mmcif- in auth numbering to do residue check for json data 
+         :returs a dictionary, where #key=chain, val= {k:auth_residue_number(withinscode),v:resname}
+        """
+        from gemmi import cif
+        doc= cif.read(self.cif_file)
+        block = doc.sole_block()
+        polyseq=block.get_mmcif_category('_atom_site.')
+
+        self.mmcif_data={} #key=chain, val= {k:auth_residue_number(withinscode),v:resname}
+        for auth_resnum,auth_resname,inscode,chain in zip(polyseq["auth_seq_id"], 
+                polyseq["auth_comp_id"],
+                polyseq["pdbx_PDB_ins_code"],
+                polyseq["auth_asym_id"]) :
+            if inscode ==False or inscode ==None:
+                inscode=""
+            auth_residue_number="%s%s" %(auth_resnum,inscode)
+            self.mmcif_data.setdefault(chain,{})[auth_residue_number]=auth_resname
+
+    def _get_residue_numbering_from_mmcif(self, chain_data) :
+
+        """Match the residues using mmcif file instead of PDBe API
+        """
+        match_flag=False
+        chain_id = chain_data["chain_label"]
+        if not "residues" in chain_data.keys():
+            return False
+        if chain_id not in self.mmcif_data :
+            mismatch= "chain %s not found in mmcif" %chain_id 
+            self.mismatches.append(mismatch)
+            return False
+        for residue in chain_data["residues"]:
+            depositor_residue_number = residue["pdb_res_label"]
+            depositor_aa_type = residue["aa_type"]
+            if depositor_residue_number in self.mmcif_data[chain_id] :
+                if self.mmcif_data[chain_id][depositor_residue_number] ==depositor_aa_type : 
+                    #print(chain_id,depositor_residue_number, depositor_aa_type)
+                    match_flag=True 
+                else :
+                    mismatch= "residue %s_%s (%s) in data does not match to %s residue in mmcif" %(chain_id,depositor_residue_number, depositor_aa_type,self.mmcif_data[chain_id][depositor_residue_number])
+                    self.mismatches.append(mismatch)
+            
+            else  :
+                mismatch= "residue %s_%s (%s) in data does not match to any residue in mmcif" %(chain_id,depositor_residue_number, depositor_aa_type)
+                self.mismatches.append(mismatch)
+
+        return match_flag
 
     def _get_residue_numbering(self, chain_data):
         """
@@ -148,9 +213,10 @@ class ResidueIndexes(object):
         :param depositor_aa_type: Residue amino acid code provided by user
         :return: True is residue numbering is valid, False if not
         """
-        for residue in residues:
+        for residue in residues:          
             if "%i%s" % (
                     residue["author_residue_number"], residue["author_insertion_code"]) == depositor_residue_number:
+                #print(depositor_residue_number,depositor_aa_type,residue["author_residue_number"], residue["author_insertion_code"])
                 return self._make_comparison(residue["residue_name"], depositor_aa_type, depositor_residue_number,
                                              depositor_chain_id)
         self.mismatches.append(
