8d5809e155b08fbd05d269580b95123cb89ed591
Lukas Polak <admin@lukaspolak.cz>
2022-10-26 18:12:09 +0200

    do not display conservation/af when N/A

--- frontend/client/viewer/components/pocket-details.tsx
+++ frontend/client/viewer/components/pocket-details.tsx
@@ -10,6 +10,12 @@ export default class PocketDetails extends React.Component
 
     constructor(props: any) {
         super(props);
+        this.checkValidValue = this.checkValidValue.bind(this);
+    }
+
+    checkValidValue(data: number | undefined) {
+        //here double equal sign is used intentionally
+        if(data == undefined || data == 0) return false;
     }
 
     render() {
@@ -20,8 +26,8 @@ export default class PocketDetails extends React.Component
                 <PocketProperty inDialog={this.props.inDialog} title="Pocket score" data={pocket.score}/>
                 <PocketProperty inDialog={this.props.inDialog} title="Probability score" data={pocket.probability || "N/A"}/>
                 <PocketProperty inDialog={this.props.inDialog} title="AA count" data={pocket.residues.length}/>
-                <PocketProperty inDialog={this.props.inDialog} title="Conservation" data={pocket.avgConservation || "N/A"}/>
-                <PocketProperty inDialog={this.props.inDialog} title="AlphaFold avg" data={pocket.avgAlphaFold || "N/A"}/>
+                {this.checkValidValue(pocket.avgConservation) && <PocketProperty inDialog={this.props.inDialog} title="Conservation" data={pocket.avgConservation!}/>}
+                {this.checkValidValue(pocket.avgAlphaFold) && <PocketProperty inDialog={this.props.inDialog} title="AlphaFold avg" data={pocket.avgAlphaFold!}/>}
                 {this.props.inDialog && <PocketProperty inDialog={this.props.inDialog} title="Residues" data={pocket.residues.join(", ")}/>}
             </div>
         );
