a827e6d88009dfa58a1ce80ddc4de952831a6e53
Lukas Polak <admin@lukaspolak.cz>
2023-03-25 14:28:10 +0100

    Docking executor code cleanup

--- executor-docking/run_task.py
+++ executor-docking/run_task.py
@@ -9,22 +9,35 @@ import enum
 import json
 import time
 
+"""
+A class to represent a task status. 
+Notice that those values are written to the info.json file that is used by the frontend,
+so any changes here should be reflected in the frontend as well.
+"""
 class Status(enum.Enum):
     QUEUED = "queued"
     RUNNING = "running"
     FAILED = "failed"
     SUCCESSFUL = "successful"
 
+"""
+Method to load a json file from a given path.
+"""
 def _load_json(path: str):
     with open(path, encoding="utf-8") as stream:
         return json.load(stream)
 
+"""
+Method to save the status file. It will update the lastChange field with the current time.
+"""
 def _save_status_file(path: str, status: any, taskId: int):
     now = datetime.datetime.today()
     status["tasks"][taskId]["lastChange"] = now.strftime('%Y-%m-%dT%H:%M:%S')
     _save_json(path, status)
 
-
+"""
+Method to save a json file to a given path.
+"""
 def _save_json(path: str, content: any):
     path_swp = path + ".swp"
     if(os.path.exists(path_swp)):
@@ -33,14 +46,20 @@ def _save_json(path: str, content: any):
         json.dump(content, stream, ensure_ascii=True)
     os.replace(path_swp, path)
 
+"""
+Method to get the path to the prediction file from the docking directory.
+"""
 def get_prediction_path(docking_directory: str):
     #currently assuming that the docking and predictions paths are different just by the name
     return os.path.join(str.replace(docking_directory, "docking", "predictions"), "public", "prediction.json")
 
+"""
+Method to execute a task for a given directory and a given taskId.
+"""
 def execute_directory_task(directory: str, taskId: int):
-    #print(os.listdir(os.path.join(directory, "..", "..", "..", "..")))
     if not os.path.exists(directory) or not os.path.isdir(directory):
         return
+    
     #first update the status file
     status_file = os.path.join(directory, "info.json")
     status = _load_json(status_file)
@@ -49,18 +68,17 @@ def execute_directory_task(directory: str, taskId: int):
     _save_status_file(status_file, status, taskId)
 
     #then load the prediction file
-    pred_path = get_prediction_path(directory)
-    prediction = _load_json(pred_path)
+    prediction = _load_json(get_prediction_path(directory))
 
-    #parse the prediction file and do some calculations
-    obj = []
+    #parse the prediction file and do some calculations - in this case just counting the number of residues per pocket
+    result = []
     for pocket in prediction["pockets"]:
-        obj.append({
+        result.append({
             "rank": pocket["rank"],
             "count": len(pocket["residues"])
         })
     
-    final_obj = json.dumps(obj)
+    result_json = json.dumps(result)
 
     #save the result file
     os.makedirs(os.path.join(directory, str(taskId), "public"), exist_ok=True)
@@ -68,25 +86,19 @@ def execute_directory_task(directory: str, taskId: int):
 
     with open(result_file, "w", encoding="utf-8") as stream:
         try:
-            stream.write(final_obj)
+            stream.write(result_json)
         finally:
             stream.flush()
     
-    #update the status file
+    #update the status file, reload it first to make sure we don't overwrite any changes
+    status = _load_json(status_file)
+    
     status["tasks"][taskId]["status"] = Status.SUCCESSFUL.value
     _save_status_file(status_file, status, taskId)
 
-    """
-    log_file = os.path.join(directory, "logTest")
-    with open(log_file, "w", encoding="utf-8") as stream:
-        try:
-            stream.write(f"Starting task at {datetime.datetime.now()}")
-            stream.write(f"Directory: {directory}")
-            stream.write(f"Prediction: {prediction}")
-        finally:
-            stream.flush()
-    """
-
+"""
+Default main method.
+"""
 def main(arguments):
     pass
 
