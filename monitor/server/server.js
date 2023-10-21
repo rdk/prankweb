40057e7c2563920ffaffdb815ab86b7513726955
Lukas Polak <admin@lukaspolak.cz>
2023-10-21 23:57:51 +0200

    Update packages & Dockerfiles

--- monitor/server/server.js
+++ monitor/server/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const logger = require("./logging");
-const request = require("request");
 
 (function main() {
   const app = express();
@@ -8,44 +7,48 @@ const request = require("request");
   start(app);
 })();
 
-function initializeApi(app) {
+async function initializeApi(app) {
   const ga = process.env.GOOGLE_ANALYTICS;
   if (ga === undefined) {
-    logger.info("No Google Analytics account provided.")
+    logger.info("No Google Analytics account provided.");
   } else {
     app.use("/api/v1/view", createOnView(ga));
   }
 }
 
 function createOnView(ga) {
-  const url = "https://www.google-analytics.com/collect?"
-    + "v=1&t=event&tid=" + ga + "&cid=0&ec=pdbe-kb&ea=view"
+  return async (req, res) => {
+    const url = "https://www.google-analytics.com/collect?" +
+      `v=1&t=event&tid=${ga}&cid=0&ec=pdbe-kb&ea=view`;
 
-  const options = {
-    "url": url,
-    "headers": {
-      "User-Agent": "request",
-    },
-  };
+    const options = {
+      method: "GET",
+      headers: {
+        "User-Agent": "request",
+      },
+    };
 
-  return (req, res) => {
-    request(options, (error, response) => {
-      if (error && response.statusCode !== 200) {
-        logger.error("Can't report view.")
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        logger.error("Can't report view.");
       }
-    });
+    } catch (error) {
+      logger.error("Error while reporting view:", error);
+    }
+
     // Do not wait for our request to finish.
-    res.status(200);
-    res.send();
-  }
+    res.sendStatus(200);
+  };
 }
 
 function start(app) {
   const port = 8021;
   app.listen(port, function onStart(error) {
     if (error) {
-      logger.error("Can't start server.", {"error": error});
+      logger.error("Can't start server.", { "error": error });
     }
-    logger.info("Server has been started.", {"port": port});
+    logger.info("Server has been started.", { "port": port });
   });
 }
